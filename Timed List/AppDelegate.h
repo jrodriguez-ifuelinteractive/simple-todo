7dea3cb1a8a37170101af02ab0cdff07eb8baae8
Jesus Rodriguez <jrodriguez@ifuelinteractive.com>
2016-09-09 21:53:26 -0400

    Initial Commit

---
+++ Timed List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timed List
+//
+//  Created by Jesus Rodriguez on 9/9/16.
+//  Copyright © 2016 Jesus Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timed List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timed List
+//
+//  Created by Jesus Rodriguez on 9/9/16.
+//  Copyright © 2016 Jesus Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
